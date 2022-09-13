a2e54aba0ee1b6ad67fd1f4409c3cb85b01f2d22
CRMclaughlin <chrisraymusik@gmail.com>
2022-09-12 19:37:48 -0500

    pseudocode

--- game.js
+++ game.js
@@ -1,1 +1,20 @@
-console.log("What up dough?")
+// define array for choices [ r, p, s]
+// define array for computerCchoice
+// define var for playerChoice
+
+//prompt() playerChoice
+
+//randomly choose computerChoice
+
+//compare choices 
+
+//display (alert) results ("won, tied, lost round")
+
+// show stats (number of wins, losses, ties)
+
+// play again?
+    // restart again
+//else 
+    //end game
+
+
