cb7d9501c035c750e1ba0127a6618866fae0252e
CRMclaughlin <chrisraymusik@gmail.com>
2022-09-24 12:23:27 -0500

    hover styles

--- game.js
+++ game.js
@@ -1,58 +1,67 @@
-
-var wins = 0
-var loss = 0
-var tie = 0
-var keepPlaying = true
-
-while(keepPlaying){
-// define array for choices [ r, p, s]
-var choices = ["r", "p", "s"]
-
-var rand = Math.floor(Math.random() * choices.length)
-
-//prompt() playerChoice
-var playerChoice = prompt("Please choose r, p, or s")
-// if (humanChoice !== "r" || !)
-
-console.log ("Player choice:", playerChoice)
-
-if (!choices.includes(playerChoice)){
-    alert("Bad Input")
-    continue;
-}
-
-// define array for computerchoice
-var computerChoice =choices [rand]
-alert("Computer chose " + computerChoice )
-
-var result
-
-if (playerChoice === computerChoice) {
-    tie++
-    result = "tied 🙈"
-} else if (
-    playerChoice === "r" && computerChoice === "s" ||    
-    playerChoice === "s" && computerChoice === "p" || 
-    playerChoice === "p" && computerChoice === "r"
-    ){
-        wins++
-        result = "Won! 🍾"
-    } else {
-        loss++
-        result = "lost 😿"
-    }
-
-alert ("You " + result)
-
-alert("Stats: \nWins: " + wins + " \nLosses " + loss + " \nTies" + tie)
+var humanScoreEl = document.getElementById("human-score")
+var botScoreEl = document.getElementById("bot-score")
+var rockImg = document.getElementById("rock")
+var paperImg = document.getElementById("paper")
+var scissorsImg = document.getElementById("scissors")
 
 
-keepPlaying = confirm("Want to play again")
-}
 
-// if playerChoice is not equal to "r" "p" "s" then print you lose
+// var choices = ["r", "p", "s"]
 
-//randomly choose computerChoice
+
+// var wins = 0
+// var loss = 0
+// var tie = 0
+// var keepPlaying = true
+
+// while(keepPlaying){
+// // define array for choices [ r, p, s]
+
+// var rand = Math.floor(Math.random() * choices.length)
+
+// //prompt() playerChoice
+// var playerChoice = prompt("Please choose r, p, or s")
+// // if (humanChoice !== "r" || !)
+
+// console.log ("Player choice:", playerChoice)
+
+// if (!choices.includes(playerChoice)){
+//     alert("Bad Input")
+//     continue;
+// }
+
+// // define array for computerchoice
+// var computerChoice =choices [rand]
+// alert("Computer chose " + computerChoice )
+
+// var result
+
+// if (playerChoice === computerChoice) {
+//     tie++
+//     result = "tied 🙈"
+// } else if (
+//     playerChoice === "r" && computerChoice === "s" ||    
+//     playerChoice === "s" && computerChoice === "p" || 
+//     playerChoice === "p" && computerChoice === "r"
+//     ){
+//         wins++
+//         result = "Won! 🍾"
+//     } else {
+//         loss++
+//         result = "lost 😿"
+//     }
+
+// alert ("You " + result)
+
+// alert("Stats: \nWins: " + wins + " \nLosses " + loss + " \nTies" + tie)
+
+
+// keepPlaying = confirm("Want to play again")
+// }
+
+// // if playerChoice is not equal to "r" "p" "s" then print you lose
+
+// //randomly choose computerChoice
 
 
     
@@ -60,17 +69,17 @@ keepPlaying = confirm("Want to play again")
 
 
     
-//compare choices 
+// //compare choices 
 
-//display (alert) results ("won, tied, lost round")
+// //display (alert) results ("won, tied, lost round")
 
 
 
-// show stats (number of wins, losses, ties)
+// // show stats (number of wins, losses, ties)
 
-// play again?
-    // restart again
-//else 
-    //end game
+// // play again?
+//     // restart again
+// //else 
+//     //end game
 
 
