2dd66273c57acde4aa773393be19ee88f044b1ce
CRMclaughlin <chrisraymusik@gmail.com>
2022-09-24 13:58:24 -0500

    9.24 completed

--- game.js
+++ game.js
@@ -1,22 +1,82 @@
 var humanScoreEl = document.getElementById('human-score')
 var botScoreEl = document.getElementById('bot-score')
-var rockImg = document.getElementById('rock')
-var paperImg = document.getElementById('paper')
-var scissorsImg = document.getElementById('scissors')
+var rockEl = document.getElementById('rock')
+var paperEl = document.getElementById('paper')
+var scissorsEl = document.getElementById('scissors')
+var resultEl = document.getElementById('result')
 
+var choicesElements = [rockEl, paperEl, scissorsEl]
 var choices = ["r", "p", "s"]
 var humanScore = 0
 var botScore = 0
+var clickable = true
+
+function resetUI() {
+    clickable = true
+    resultEl.innerText = "Rock Paper Scissors"
+    choicesElements.forEach(function(el) {
+        el.style.display = 'block'
+        el.classList.remove('loser')
+        var spans = el.querySelectorAll('span')
+        for (var i= 0; i < spans.length; i++) {
+            spans[i].remove()
+        }
+
+    })
+}
+
+function updateScores() {
+  humanScoreEl.innerText = humanScore
+  botScoreEl.innerText = botScore
+}
+
+function showResults(humanChosenEl, computerChosenEl, result) {
+    clickable = false
+  resultEl.innerText = result
+  updateScores()
+  // hide all three
+  choicesElements.forEach(function(el) {
+    el.style.display = 'none'
+    el.setAttribute('disabled', '')
+  })
+  humanChosenEl.style.display = "block"
+  computerChosenEl.style.display = "block"
+
+  var humanSpan = document.createElement('span')
+  humanSpan.innerText = "You"
+  humanChosenEl.appendChild(humanSpan)
+
+  var botSpan = document.createElement('span')
+  botSpan.innerText = "Bot"
+  computerChosenEl.appendChild(botSpan)
+
+  if (result === 'YOU WON!') {
+    computerChosenEl.classList.add('loser')
+  }else if (result === 'BOT WON!')
+    humanChosenEl.classList.add('loser')
+
+setTimeout(function(){
+    resetUI()
+}, 3000)
+}
 
 function startRound(event) {
-  var humanChosenImg = event.target
-  var humanChoice = humanChosenImg.dataset.letter  
+    if (!clickable) return
+  var humanChosenEl
+  // make sure we're referring to the correct element
+  if (event.target.matches('img')) {
+    humanChosenEl = event.target.parentElement
+  } else {
+    humanChosenEl = event.target
+  }
+
+  var humanChoice = humanChosenEl.dataset.letter
   
   // randomly choose computerChoice
   var random = Math.floor(Math.random() * choices.length)
   var computerChoice = choices[random]
-  var computerChosenImg = document.querySelector('img[data-letter="'+ computerChoice + '"]')
-  
+  var computerChosenEl = document.querySelector('[data-letter="'+ computerChoice +'"]')
+
   var result
   if (humanChoice === computerChoice) {
     result = "TIED!"
@@ -30,26 +90,11 @@ function startRound(event) {
   } else {
     botScore++
     result = "BOT WON!"
-}
-[rockImg, paperImg, scissorsImg].forEach(function(img){
-    img.style.display = 'none'
-})
-humanChosenImg.style.display = 'block'
-computerChosenImg.style.display = 'block'
+  }
+
+ showResults(humanChosenEl, computerChosenEl, result)
 }
 
-
-//   // display (alert) comparison results (won, tied, lost round)
-//   alert("You " + result)
-
-//   // show stats
-//   alert("Stats:\nWins: " + wins + "\nLosses: " + losses + "\nTies: " + ties)
-
-//   keepPlaying = confirm("Want to play again?")
-// }
-
-rockImg.addEventListener('click', startRound)
-paperImg.addEventListener('click', startRound)
-scissorsImg.addEventListener('click', startRound)
-
-
+rockEl.addEventListener('click', startRound)
+paperEl.addEventListener('click', startRound)
+scissorsEl.addEventListener('click', startRound)
